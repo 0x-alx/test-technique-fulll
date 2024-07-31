e56a342f90f7156680bb79b23513fb1807f807c4
0x-alx <alx.charbo@gmail.com>
2024-07-31 13:32:13 +0200

    Navbar added to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { Navbar } from "./components";
 
 function App() {
-	return <div className='App'>App</div>;
+	return (
+		<div className='App'>
+			<Navbar />
+		</div>
+	);
 }
 
 export default App;
