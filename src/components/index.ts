56b361d4ec83fd9606163c37f19a951764d84256
0x-alx <alx.charbo@gmail.com>
2024-07-31 13:30:46 +0200

    Main components added

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+import { Input } from "./Input/Input";
+import { Navbar } from "./Navbar/Navbar";
+import { Avatar } from "./Avatar/Avatar";
+import { Checkbox } from "./Checkbox/Checkbox";
+import { Button } from "./Button/Button";
+import { Card } from "./Card/Card";
+
+export { Input, Navbar, Avatar, Checkbox, Button, Card };
